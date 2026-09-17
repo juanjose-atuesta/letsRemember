14bdf0f49e93b0fd4b6776ef03deb02cff3e2a7a
juanjo <juanjoseatuestaflor@gmail.com>
2026-09-16 22:09:09 -0500

    terminamos el storage del schelude

--- backend/store/scheludeStore.go
+++ backend/store/scheludeStore.go
@@ -5,27 +5,23 @@ import (
 	"database/sql"
 )
 
+type ScheludeStore interface {
+	GetAll() ([]*models.Schedule, error)
 
-type ScheludeStore interface{
-	GetALl() ([]*models.Schedule)
-	
 	Create(information *models.Schedule) (*models.Schedule, error)
 	Update(information *models.Schedule, id int) error
 	Delete(id int) error
 }
 
-
-
-type store struct {
+type storeSc struct {
 	db *sql.DB
 }
 
-func New(db *sql.DB) ScheludeStore {
-	return &store{db: db}
+func News(db *sql.DB) ScheludeStore {
+	return &storeSc{db: db}
 }
 
-
-func (s* store) GetALl() ([]*models.Schedule, error) {
+func (s *storeSc) GetAll() ([]*models.Schedule, error) {
 
 	q := " SELECT id, day, eventDescription FROM SCHELUDE"
 	rows, err := s.db.Query(q)
@@ -34,7 +30,7 @@ func (s* store) GetALl() ([]*models.Schedule, error) {
 	}
 	defer rows.Close()
 
-		var information = []*models.Schedule{}
+	var information = []*models.Schedule{}
 	for rows.Next() {
 		b := &models.Schedule{}
 		if err := rows.Scan(&b.Id, &b.Day, &b.EventDescription); err != nil {
@@ -45,14 +41,42 @@ func (s* store) GetALl() ([]*models.Schedule, error) {
 	}
 	return information, err
 
-
-	
 }
 
+func (s *storeSc) Create(information *models.Schedule) (*models.Schedule, error) {
+	q := "INSERT INTO SCHELUDE (day, eventDescription) VALUES (?,?)"
+	res, err := s.db.Exec(q, information.Day, information.EventDescription)
 
-func (s *store) Create(information *models.Schedule) (*models.Schedule, error){
-	q:= ""
+	if err != nil {
+		return nil, err
+	}
+	id, err := res.LastInsertId()
 
+	if err != nil {
+		return nil, err
+	}
 
+	information.Id = int(id)
+	return information, nil
 
+}
+
+func (s *storeSc) Update(information *models.Schedule, id int) error {
+	q := "UPDATE SCHELUDE SET day = ?, eventDescription = ? WHERE id = ? "
+	_, err := s.db.Exec(q, information.Day, information.EventDescription, id)
+
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (s *storeSc) Delete(id int) error {
+	q := "DELETE FROM SCHELUDE WHERE id = ?"
+	_, err := s.db.Exec(q, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
